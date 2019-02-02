8b3fc10f9f4aa69d66f27737173b62c9a44d3cca
Aaron D. Milstein <neurosutras@gmail.com>
2019-02-02 13:08:15 -0800

    20190202: Adding cori jobscript for test_pc_submit_limits

---
+++ test_pc_submit_limits_cori.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#SBATCH -J test_pc_submit_limits_20190202
+#SBATCH -o logs/test_pc_submit_limits_20190202.%j.o
+#SBATCH -e logs/test_pc_submit_limits_20190202.%j.e
+#SBATCH -q debug
+#SBATCH -N 1
+#SBATCH -L SCRATCH
+#SBATCH -C haswell
+#SBATCH -t 00:30:00
+
+set -x
+
+cd $HOME/neuron_pc_extension
+
+srun -N 1 -n 32 -c 2 --cpu_bind=cores python test_pc_submit_limit.py --block-size=10000 --task-limit=300000
