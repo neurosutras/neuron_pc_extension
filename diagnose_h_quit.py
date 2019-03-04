f9402cac95a8484966429713726190954e0a311d
Aaron D. Milstein <neurosutras@gmail.com>
2019-03-04 09:20:30 -0800

    20190304: - Testing master branch of nrn on Cori.

--- diagnose_h_quit.py
+++ diagnose_h_quit.py
@@ -9,65 +9,48 @@ from neuron import h
 print('diagnoise_h_quit: getting past from neuron import h')
 sys.stdout.flush()
 time.sleep(1.)
-import click
 
 comm = MPI.COMM_WORLD
 
+h.nrnmpi_init()
+print('diagnoise_h_quit: getting past h.nrnmpi_init()')
+sys.stdout.flush()
+time.sleep(1.)
 
-@click.command(context_settings=dict(ignore_unknown_options=True, allow_extra_args=True))
-@click.option('--use-subworlds', is_flag=True)
-@click.option('--call-pc-done', is_flag=True)
-@click.option('--procs-per-worker', type=int, default=1)
-def main(use_subworlds, call_pc_done, procs_per_worker):
-    """
+pc = h.ParallelContext()
+pc.subworlds(1)
+global_rank = int(pc.id_world())
+global_size = int(pc.nhost_world())
+rank = int(pc.id())
+size = int(pc.nhost())
+print('diagnoise_h_quit: process id: %i; global rank: %i / %i; local rank: %i / %i; mpi comm rank: %i / %i' %
+      (os.getpid(), global_rank, global_size, rank, size, comm.rank, comm.size))
+sys.stdout.flush()
+time.sleep(1.)
 
-    :param use_subworlds: bool
-    :param call_pc_done: bool
-    :param procs_per_worker: int
-    """
-    h.nrnmpi_init()
-    print('diagnoise_h_quit: getting past h.nrnmpi_init()')
+
+pc.runworker()
+print('diagnoise_h_quit: got past pc.runworker()')
+sys.stdout.flush()
+time.sleep(1.)
+
+# catch workers escaping from runworker loop
+if global_rank != 0:
+    print('diagnoise_h_quit: global_rank: %i escaped from the pc.runworker loop')
     sys.stdout.flush()
     time.sleep(1.)
+    os._exit(1)
 
-    pc = h.ParallelContext()
-    if use_subworlds:
-        pc.subworlds(procs_per_worker)
-    global_rank = int(pc.id_world())
-    global_size = int(pc.nhost_world())
-    rank = int(pc.id())
-    size = int(pc.nhost())
-    print('diagnoise_h_quit: process id: %i; global rank: %i / %i; local rank: %i / %i' %
-          (os.getpid(), global_rank, global_size, rank, size))
-    sys.stdout.flush()
-    time.sleep(1.)
+pc.done()
+print('Got past pc.done()')
+sys.stdout.flush()
+time.sleep(1.)
+os._exit(1)
 
-    print('diagnoise_h_quit: process id: %i; mpi rank: %i / %i' % (os.getpid(), comm.rank, comm.size))
-    sys.stdout.flush()
-    time.sleep(1.)
-
-    pc.runworker()
-    print('diagnoise_h_quit: got past pc.runworker()')
-    sys.stdout.flush()
-    time.sleep(1.)
-
-    # catch workers escaping from runworker loop
-    if global_rank != 0:
-        print('diagnoise_h_quit: global_rank: %i escaped from the pc.runworker loop')
-        sys.stdout.flush()
-        time.sleep(1.)
-        os._exit(1)
-
-    if call_pc_done:
-        pc.done()
-
-    print('calling h_quit')
-    sys.stdout.flush()
-    time.sleep(1.)
-    h.quit()
-    print('Got past h_quit')
-    sys.stdout.flush()
-    time.sleep(1.)
-
-if __name__ == '__main__':
-    main(standalone_mode=False)
+print('calling h_quit()')
+sys.stdout.flush()
+time.sleep(1.)
+h.quit()
+print('Got past h_quit()')
+sys.stdout.flush()
+time.sleep(1.)
